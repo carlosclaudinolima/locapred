69e16ae86d0cb639305269042edf1ae88ddc8f93
carlosclaudinolima <carlosclaudinolima@gmail.com>
2026-05-24 11:58:31 +0000

    Finishing silver transformations

--- src/01_pipelines/02_processamento_silver.py
+++ src/01_pipelines/02_processamento_silver.py
@@ -6,31 +6,38 @@
 #        'solucao', 'aberto_por', 'incidente_pai', 'status', 'entrou_para_kpi',
 #        'kpi_violado'
 
-
 from pyspark.sql import functions as F
 
-# Lendo da Bronze
-df_bronze = spark.table("bronze.incidentes_locaweb")
+# 1. Leitura do Bronze
+df_silver = spark.table("bronze.incidentes_locaweb")
 
-# 1. Limpeza, Tipagem Forte e Normalização
-df_silver = df_bronze \
+# 2. Higienização de Datas e Engenharia de Sazonalidade (Para o XGBoost capturar padrões temporais)
+df_silver = df_silver \
     .withColumn("aberto_ts", F.to_timestamp(F.col("aberto"), "dd/MM/yyyy HH:mm:ss")) \
-    .withColumn("resolvido_ts", F.to_timestamp(F.col("resolvido"), "dd/MM/yyyy HH:mm:ss")) \
-    .withColumn("encerrado_ts", F.to_timestamp(F.col("encerrado"), "dd/MM/yyyy HH:mm:ss")) \
-    .withColumn("duracao_horas", F.col("duracao") / 3600.0)
+    .withColumn("data_abertura", F.to_date(F.col("aberto_ts"))) \
+    .withColumn("mes", F.month(F.col("aberto_ts"))) \
+    .withColumn("dia_semana", F.dayofweek(F.col("aberto_ts"))) \
+    .withColumn("hora_dia", F.hour(F.col("aberto_ts"))) \
+    .withColumn("is_fim_semana", F.when(F.dayofweek(F.col("aberto_ts")).isin([1, 7]), 1).otherwise(0))
 
-# 2. Features Lógicas Baseadas nas Regras de Negócio Locaweb
+# 3. Aplicação rigorosa das regras de Negócio Locaweb
 df_silver = df_silver \
     .withColumn("valido_kpi", 
-                F.when(
-                    (F.col("status") != "Sem Intervenção") &  # Status Sem Intervenção não entra no KPI [cite: 474]
-                    (F.col("incidente_pai").isNull()) &       # Valor preenchido não entra no KPI [cite: 473]
-                    (F.col("prioridade").isin(["1 - Crítica", "2 - Alta", "3 - Média"])), # Apenas P1, P2 e P3 [cite: 466]
-                    True
-                ).otherwise(False)) \
-    .withColumn("data_abertura", F.to_date(F.col("Aberto_ts")))
+        F.when(
+            (F.col("status") != "Sem Intervenção") & 
+            (F.col("incidente_pai").isNull()) &      
+            (F.col("prioridade").isin(["1 - Crítica", "2 - Alta", "3 - Média"])), 
+            True
+        ).otherwise(False)
+    ) \
+    .withColumn("ola_violado",
+        F.when(F.col("valido_kpi") == False, False)
+         # Duração está em segundos. P1 e P2 = até 4h (14400s) 
+         .when(F.col("prioridade").isin(["1 - Crítica", "2 - Alta"]) & (F.col("duracao") > 14400), True)
+         # P3 = até 12h (43200s)
+         .when((F.col("prioridade") == "3 - Média") & (F.col("duracao") > 43200), True)
+         .otherwise(False)
+    )
 
-# Salvando na Silver
-df_silver.write.format("delta") \
-    .mode("overwrite") \
-    .saveAsTable("silver.incidentes_locaweb_tratados")
+# Persistindo a Silver
+df_silver.write.format("delta").mode("overwrite").saveAsTable("silver.incidentes_locaweb_tratados")
